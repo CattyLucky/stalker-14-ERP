aac9f0187e387c618437c0e240f8d2ead9d79394
FrauzJ <mihailpavlichenko2006@gmail.com>
2026-02-22 13:16:14 +0200

    ritual chasm not done but ez

---
+++ Content.Shared/_Stalker_EN/RitualChasm/RitualChasmComponent.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.RitualChasm;
+
+/// <summary>
+///     Marks a component that will cause entities to fall into them on a step trigger activation
+/// </summary>
+[NetworkedComponent, RegisterComponent, Access(typeof(RitualChasmSystem))]
+public sealed partial class RitualChasmComponent : Component
+{
+    /// <summary>
+    ///     Sound that should be played when an entity falls into the chasm
+    /// </summary>
+    [DataField("fallingSound")]
+    public SoundSpecifier FallingSound = new SoundPathSpecifier("/Audio/Effects/falling.ogg");
+}
